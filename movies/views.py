fe1b11227bbeba7fc3f1e6e5c1507ae9a7f0ac21
Raccoonrocket <9802432172sergei@gmail.com>
2022-10-03 19:15:53 +0300

    Add project, step 9 done

--- movies/views.py
+++ movies/views.py
@@ -1,8 +1,10 @@
-from django.shortcuts import render
+import requests
+from django.shortcuts import render, redirect
 from django.views.generic import ListView, DetailView
 from django.views.generic.base import View
 
 from .models import Movie
+from .forms import ReviewsForm
 
 
 class MoviesView(ListView):
@@ -16,3 +18,15 @@ class MovieDetailView(DetailView):
     """Полное описание фильма"""
     model = Movie
     slug_field = "url"
+
+
+class AddReviews(View):
+    """Отзывы"""
+    def post(self, request, pk):
+        form = ReviewsForm(request.POST)
+        movie = Movie.objects.get(id=pk)
+        if form.is_valid():
+            form = form.save(commit=False)
+            form.movie_id = pk
+            form.save()
+        return redirect(movie.get_absolute_url())
